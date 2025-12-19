7cf01581018b5f9840586c57feadfde69259931a
Saurav Khare <saurav.erahk@gmail.com>
2025-12-20 04:32:02 +0530

    fix: fixed eden host issues

--- lib/eden.ts
+++ lib/eden.ts
@@ -1,5 +1,10 @@
 import { treaty } from "@elysiajs/eden";
 import type { app } from "../app/api/[[...slugs]]/route";
 
+const host =
+  process.env.NODE_ENV === "development"
+    ? "localhost:3000"
+    : "poof-rho.vercel.app";
+
 // this require .api to enter /api prefix
-export const eden = treaty<app>("localhost:3000").api;
+export const eden = treaty<app>(host).api;
